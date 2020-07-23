5c1485ea1c07b6d4768b3538880f836929b190ec
Waylon Walker <quadmx08@gmail.com>
2020-07-22 21:46:35 -0500

    Switch to https://waylonwalker.com

--- sample.py
+++ sample.py
@@ -3,13 +3,9 @@ from src.testproject.sdk.drivers import webdriver
 if __name__ == "__main__":
     driver = webdriver.Chrome()
 
-    driver.get("https://example.testproject.io/web/")
+    driver.get("https://waylonwalker.com/")
 
-    driver.find_element_by_css_selector("#name").send_keys("John Smith")
-    driver.find_element_by_css_selector("#password").send_keys("12345")
-    driver.find_element_by_css_selector("#login").click()
-
-    passed = driver.find_element_by_css_selector("#logout").is_displayed()
+    passed = driver.find_element_by_css_selector("section.about").is_displayed()
 
     print("Test passed") if passed else print("Test failed")
 
