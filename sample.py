5863bd70d404fc80f4c0351123594d374e0cd251
Waylon Walker <quadmx08@gmail.com>
2020-07-24 23:33:07 -0500

    Update sample.py

--- sample.py
+++ sample.py
@@ -4,9 +4,11 @@ if __name__ == "__main__":
     driver = webdriver.Chrome()
 
     driver.get("https://waylonwalker.com/")
-
-    passed = driver.find_element_by_css_selector(".about").is_displayed()
-
+    headers = driver.find_elements_by_tag_name("h2")
+    
+    passed = headers[0].is_displayed()
+    
+    print('number of headers: ', len(headers))
     print("Test passed") if passed else print("Test failed")
 
     driver.quit()
